323e8c65cf58e31c41b23d8584b57178734a587e
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-08-19 16:04:25 -0700

    Initial commit.

---
+++ src/main/java/com/example/mongooseVersusCobra/MongooseVersusCobraApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongooseVersusCobra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongooseVersusCobraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongooseVersusCobraApplication.class, args);
+	}
+
+}
